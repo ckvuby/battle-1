19d7dddeabf08a3147cf54767ffaf5e2b28066a8
Patrick Harris <patricknpharris@gmail.com>
2018-09-18 16:23:03 +0100

    p2 hit points are visible

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -4,3 +4,17 @@ feature 'Testing infrastructure' do
     expect(page).to have_content("Testing infrastructure working!")
   end
 end
+
+feature 'Enter names' do
+  scenario 'players enter names, submit and see names on screen' do
+    sign_in_and_play
+    expect(page).to have_content('Patrick', 'Chloe')
+  end
+end
+
+feature 'Hit Points' do
+  scenario "Player 2's hit points are visible" do
+    sign_in_and_play
+    expect(page).to have_content 'Chloe: 600HP'
+  end
+end
